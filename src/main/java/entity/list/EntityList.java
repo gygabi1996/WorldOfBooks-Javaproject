cbf49bba21bf2a0c56e6bb681f13b8ae7512f733
Győrössy Gábor <gyorossygabi@gmail.com>
2020-12-22 20:15:59 +0100

    Create EntityList
    Create EntityList class which contain all the entities got from the API and store in different lists

---
+++ src/main/java/entity/list/EntityList.java
@@ -0,0 +1,18 @@
+package main.java.entity.list;
+
+import main.java.entity.Listing;
+import main.java.entity.ListingStatus;
+import main.java.entity.Location;
+import main.java.entity.Marketplace;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class EntityList {
+    public Map<Listing,Boolean> listingList = new HashMap<>();
+    public Set<Location> locationList = new HashSet<>();
+    public Set<ListingStatus> listingStatuseList = new HashSet<>();
+    public Set<Marketplace> marketplaceList = new HashSet<>();
+}
