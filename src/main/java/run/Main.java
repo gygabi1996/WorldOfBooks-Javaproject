15b2f412277339ae55eeeeb851fc3e984047bf31
Győrössy Gábor <gyorossygabi@gmail.com>
2020-12-22 17:28:59 +0100

    Create Main.java
    Create empty Main.java

---
+++ src/main/java/run/Main.java
@@ -0,0 +1,24 @@
+package main.java.run;
+
+import com.google.gson.Gson;
+import com.mashape.unirest.http.ObjectMapper;
+import main.java.api.LocationApiHandler;
+import main.java.api.MainApiHandler;
+import main.java.entity.Location;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
