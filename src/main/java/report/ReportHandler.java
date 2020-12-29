de0efa90c5c52da0af07f36764fede0e8c0de4c2
Győrössy Gábor <gyorossygabi@gmail.com>
2020-12-29 13:23:11 +0100

    Prettify report.JSON
    Prettify report.JSON

--- src/main/java/report/ReportHandler.java
+++ src/main/java/report/ReportHandler.java
@@ -3,6 +3,7 @@ package main.java.report;
 
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import main.java.entity.ReportEntity;
 import org.json.JSONObject;
 
@@ -12,7 +13,7 @@ import java.util.Map;
 
 public class ReportHandler {
     public static void createReport(ReportEntity reportEntity){
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter writer = new FileWriter("json/report.JSON")) {
             gson.toJson(reportEntity, writer);
         } catch (IOException e) {
